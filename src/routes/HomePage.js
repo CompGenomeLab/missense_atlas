fc00df450a9b058ae3a519ddc1698cfd342da085
Murat Aslan <muratkerimaslan@gmail.com>
2022-05-19 20:57:30 +0300

    first version

---
+++ src/routes/HomePage.js
@@ -0,0 +1,30 @@
+
+
+// import HeatMap from "../components/HeatMap";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+
+
+    return(
+        <>
+            <h1>
+                Welcome
+            
+               
+                    <Link to ='/protein/TSBP1'> go to TSBP1 </Link>
+        
+            </h1>
+        
+        
+        
+        </>
+
+
+    )
+
+}
+
+
+export default HomePage;
