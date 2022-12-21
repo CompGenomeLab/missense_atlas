a9243c02cc91eb2d4e3e48017fe681f615a8c9a0
Murat Aslan <muratkerimaslan@gmail.com>
2022-12-21 21:02:29 +0300

    Searchbox sequence detection is better now, only Lines full of valid aminoacids are considered part of the sequence

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -6,6 +6,8 @@ import CloseIcon from '@mui/icons-material/Close';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
+const aminoacid_ordering = ['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V'];
+
 function SearchBox(){
 
     const navigate = useNavigate();
@@ -14,8 +16,42 @@ function SearchBox(){
     const [proteinSequence,setProteinSequence] = useState(''); // .trim() the sequence to remove white spaces
     const [inputMD5Sum , setInputMD5Sum] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    let trimmedProteinSequence = proteinSequence; // will be trimmed using helper_protein_sequence_trimmer
     // let test_hash = MD5(protein_sequence).toString();
     
+    // doesn't actually need to take sequence as input, as proteinSequence is reachable 
+    // asd D  ARNDC Q E G H I J K L M N
+    const helper_protein_sequence_trimmer = () => {
+      let trimmedSequence = "";
+      let string_lines = proteinSequence.split('\n');
+      for(let line of string_lines){
+        let cur_line = line.trim();
+        let is_sequence_line_flag = true;
+        let cur_line_aminoacids = "";
+        for (let char of cur_line){
+          if(char !== ' '){// ignoring whitesapces
+            if (aminoacid_ordering.indexOf(char) === -1) // not an aminoacid symbol
+            {
+              is_sequence_line_flag = false;
+            }
+            else{ 
+              cur_line_aminoacids += char;
+            }
+          }
+        }
+        if (is_sequence_line_flag){
+          trimmedSequence += cur_line_aminoacids
+        }
+      }
+      return trimmedSequence;
+      // for(let i = 0; i < proteinSequence.length ; i++)
+      // {
+      //   if(proteinSequence[i] === '\n')
+      //   console.log(i, proteinSequence[i]);
+      // }
+    }
+
+    trimmedProteinSequence = helper_protein_sequence_trimmer();
     // console.log("test_hash = " + test_hash);
     // console.log("method = ");
     // console.log(searchMethod);
@@ -41,14 +77,13 @@ function SearchBox(){
         else if (searchMethod === 'Sequence'){ 
             return(
                 <div style={{ margin:'2rem 0px'}}>
-                    <h4 style={{margin:'0.5rem 0px'}}> Sequence Length: {proteinSequence.trim().length}</h4>
+                    <h4 style={{margin:'0.5rem 0px'}}> Sequence Length: {trimmedProteinSequence.length}</h4>
                     <textarea 
                     style={{width:'50%',height:'30vh'}}
                     placeholder="protein Sequence"
                     value={proteinSequence}
                     onChange={(e) => setProteinSequence(() => e.target.value)}
                     >
-
                     </textarea>
                 </div>
             )
@@ -88,7 +123,7 @@ function SearchBox(){
     const handleSearchClicked = () => {
         let final_md5Sum = inputMD5Sum;
         if (searchMethod === 'Sequence'){
-            final_md5Sum = MD5(proteinSequence.trim()).toString();
+            final_md5Sum = MD5(trimmedProteinSequence).toString();
             // console.log(final_md5Sum);
         }
         callApi(final_md5Sum);
