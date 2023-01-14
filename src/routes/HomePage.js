c53cc19338c7f8921e0ea7824e65ded6a55d9673
Murat Aslan <muratkerimaslan@gmail.com>
2023-01-14 23:29:05 +0300

    Main page minor fix

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -4,13 +4,14 @@ import { h1_font_size } from "../config/config";
 
 const HomePage = () => {
   return (
-    <div style={{margin:"0px auto", width:"max-content"}}>
+    <div >
       <h1 style={{textAlign:'center', fontSize:h1_font_size}}>
         Missense Atlas
       </h1>
       <div >
         <SearchBox />
       </div>
+      
     </div>
   );
 };
